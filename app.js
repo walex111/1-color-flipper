62cb3da1e2249b2dd3a2131b628010931561c0ef
Samson T. Oketunmbi <oyattunechi@gmail.com>
2020-08-19 13:09:48 -0600

    app, hex, index

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const colors = ["grey", "black", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById('btn');
+const header = document.querySelector('.header');
+const homeColorChange = document.querySelector('.home-color-change');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
@@ -7,6 +9,8 @@ btn.addEventListener('click', function () {
     console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
+    header.style.color = colors[randomNumber];
+    homeColorChange.style.color = colors[randomNumber];
 });
 
 function getRandomNumber() {
